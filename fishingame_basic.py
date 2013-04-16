4c350ea4400feb9f4ee6744159ebe8f345edab2f
tehChorbs <cuchonchuir@gmail.com>
2013-04-16 15:26:33 -0700

    Create fishingame_basic.py

---
+++ fishingame_basic.py
@@ -0,0 +1,26 @@
+lake_fish = ["Brown Trout", "Rainbow Trout", "Lake Trout", "Largemouth Bass", "Black Crappie", "Tench", "Common Carp", "Roach", "Common Bream", "Rudd",
+"Wels Catfish", "Blue Catfish", "Channel Catfish", "Zander", "European Perch"]
+
+river_fish = ["Brown Trout", "Rainbow Trout", "Largemouth Bass", "White Crappie", "Barbel", "European Chub", "Wels Catfish", "Blue Catfish", "Channel Catfish", "Zander", "Striped Bass", "Gray Mullet"]
+
+beach_fish = ["Sea Trout", "Steelhead", "Ling", "Coalfish", "Pollack", "Sea Bass", "Conger Eel", "Ballan Wrasse", "Red Drum", "Black Drum", "Bull Huss", "Winter Flounder", "European Plaice", "Turbot", "Striped Bass", "Gray Mullet", "Black Seabream", "Common Skate", "Blonde Ray", "Bluefish", "Tarpon"]
+
+menu_item = 0
+while menu_item !=("Q"):
+    print "Where would you like to fish?  At the Lake, the River, or the Beach?"
+    print "To quit, press Q."
+    menu_item = raw_input("Pick an option: ")
+
+    if menu_item == ("Lake"):
+        from random import choice
+        print "You caught a ", choice(lake_fish), "!"
+
+    elif menu_item == ("River"):
+        from random import choice
+        print "You caught a ", choice(river_fish), "!"
+        
+    elif menu_item == ("Beach"):
+        from random import choice
+        print "You caught a ", choice(beach_fish), "!"
+     
+print "Goodbye."
