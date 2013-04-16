ad7e19d0f74d9dad0cb699a1a7e50bd564d025b3
tehChorbs <cuchonchuir@gmail.com>
2013-04-16 15:49:00 -0700

    Update fishingtest4.py

--- fishingtest4.py
+++ fishingtest4.py
@@ -1,3 +1,11 @@
+#This was originally intended to be a definition, but it wasn't working properly . . .  It's just printing 
+#the choice and not giving the further options.  The "while" near the top was to hopefully make a loop.
+
+#My goal if this worked was to then have it go into a new definition that functioned similarly, wherein 
+#the fish did such actions as run, get tired, jump, et cetera, and you would have to react appropriately, 
+#otherwise the fish would get away.  At its most basic it'd a be a trial-and-error memory game, but that's
+#about the limit of my ability atm, though I have some thoughts for interesting ideas in the future.
+
 line_cast = ["The float bobs down slightly", "The float disappears under the water", "The float stays serenely still"]
 
 print "You cast your line into the water."
