64a7b8f97aa6855ce393e8c7278e5480109dd4b2
tehChorbs <cuchonchuir@gmail.com>
2013-04-16 15:43:41 -0700

    Update fishingtest2.py

--- fishingtest2.py
+++ fishingtest2.py
@@ -1,3 +1,8 @@
+#My second version, wherein I tried to give a possible negative result (an old boot instead of a fish).  It's 
+#still just random, though.  I shortened the list of fish to just make it more likely to get the boot so I 
+#could know if it was working properly.
+
+#Also began to research using weighted lists; it sounds very complex right now.
 lake_fish = ["Trout", "Old Boot"]
 
 river_fish = ["Trout", "Salmon"]
