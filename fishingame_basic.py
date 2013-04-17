586224f5746d8b6dae88b5573d128970e8618c92
tehChorbs <cuchonchuir@gmail.com>
2013-04-16 19:21:28 -0700

    Update fishingame_basic.py

--- fishingame_basic.py
+++ fishingame_basic.py
@@ -6,6 +6,8 @@ river_fish = ["Brown Trout", "Rainbow Trout", "Largemouth Bass", "White Crappie"
 
 beach_fish = ["Sea Trout", "Steelhead", "Ling", "Coalfish", "Pollack", "Sea Bass", "Conger Eel", "Ballan Wrasse", "Red Drum", "Black Drum", "Bull Huss", "Winter Flounder", "European Plaice", "Turbot", "Striped Bass", "Gray Mullet", "Black Seabream", "Common Skate", "Blonde Ray", "Bluefish", "Tarpon"]
 
+size = ["tiny", "small", "average-sized", "large", "giant"]
+
 menu_item = 0
 while menu_item !=("Q"):
     print "Where would you like to fish?  At the Lake, the River, or the Beach?"
@@ -14,14 +16,14 @@ while menu_item !=("Q"):
 #Okay, so there's not much gameplay atm.  It just picks a random fish.  I'm working on adding some gameplay elements.
     if menu_item == ("Lake"):
         from random import choice
-        print "You caught a ", choice(lake_fish), "!"
+        print "You caught a", choice(size), choice(lake_fish), "!"
 
     elif menu_item == ("River"):
         from random import choice
-        print "You caught a ", choice(river_fish), "!"
+        print "You caught a", choice(size), choice(river_fish), "!"
         
     elif menu_item == ("Beach"):
         from random import choice
-        print "You caught a ", choice(beach_fish), "!"
-        
+        print "You caught a", choice(size), choice(beach_fish), "!"
+     
 print "Goodbye."
